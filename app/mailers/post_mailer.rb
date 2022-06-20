abda88aed53431836321f51afe8e60933326869d
abdulQudoos7 <abdul.qudoos@southvillesol.com>
2022-06-20 12:49:14 +0500

    second commit

---
+++ app/mailers/post_mailer.rb
@@ -0,0 +1,14 @@
+class PostMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.post_mailer.post_created.subject
+  #   en.post_mailer.post_created.subject
+  #
+  def post_created
+    @greeting = "Hi"
+
+    mail to: 'reciverrr@gmail.com'
+  end
+end
